360c2a09399bdbac1c9d3ee9bd9acadc999a7def
Andrew Laustrup <83102227+ALaustrup@users.noreply.github.com>
2025-12-26 10:18:59 -0600

    Fix: await handler import in serve.js

--- apps/web/serve.js
+++ apps/web/serve.js
@@ -1,10 +1,12 @@
 import { serve } from "@hono/node-server";
-import handler from "./build/server/index.js";
+import serverModule from "./build/server/index.js";
 
 const port = Number(process.env.PORT) || 4000;
 
+const handler = await serverModule.default;
+
 serve({
-  fetch: handler,
+  fetch: handler.fetch,
   port,
 });
 
