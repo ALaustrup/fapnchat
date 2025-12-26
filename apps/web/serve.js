a09b7acd159971f125e81002e4633b10828508aa
Andrew Laustrup <83102227+ALaustrup@users.noreply.github.com>
2025-12-26 10:23:15 -0600

    Add error handlers and delay exit

--- apps/web/serve.js
+++ apps/web/serve.js
@@ -1,21 +1,31 @@
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('UNHANDLED REJECTION:', reason);
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('UNCAUGHT EXCEPTION:', error);
+});
+
 console.log("SCRIPT STARTED");
 
 setTimeout(async () => {
   try {
     console.log("BEFORE IMPORT");
     const mod = await import("./build/server/index.js");
-    console.log("AFTER IMPORT");
+    console.log("AFTER IMPORT - SUCCESS");
     
     console.log("EXPORT KEYS:", Object.keys(mod));
     console.log("DEFAULT TYPE:", typeof mod.default);
     
     if (mod.default) {
       console.log("DEFAULT VALUE TYPE:", typeof mod.default);
-      if (typeof mod.default === "object") {
+      if (typeof mod.default === "object" && mod.default !== null) {
         console.log("DEFAULT KEYS:", Object.keys(mod.default));
         if (mod.default.fetch) {
           console.log("DEFAULT HAS FETCH");
         }
+      } else if (typeof mod.default === "function") {
+        console.log("DEFAULT IS FUNCTION");
       }
     }
     
@@ -23,11 +33,11 @@ setTimeout(async () => {
       console.log("NAMED FETCH EXPORT");
     }
     
-    process.exit(0);
+    setTimeout(() => process.exit(0), 100);
   } catch (error) {
     console.error("ERROR:", error.message);
     console.error(error.stack);
-    process.exit(1);
+    setTimeout(() => process.exit(1), 100);
   }
 }, 100);
 
