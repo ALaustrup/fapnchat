9a62eaf44d68dfd9cff59c7303193a9ce161eb25
Andrew Laustrup <83102227+ALaustrup@users.noreply.github.com>
2025-12-26 14:27:43 -0600

    fix(build): remove catch-all route from prerender to fix Windows build
    - Remove '/*?' from prerender config (invalid Windows filename)
    - Catch-all route will render on-demand instead
    - Fixes ENOENT error during build on Windows

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -3,5 +3,7 @@ import type { Config } from '@react-router/dev/config';
 export default {
 	appDirectory: './src/app',
 	ssr: true,
-	prerender: ['/*?'],
+	// Alpha: Don't prerender catch-all route (*?) - causes Windows filename issues
+	// The not-found page will be rendered on-demand
+	prerender: [],
 } satisfies Config;
