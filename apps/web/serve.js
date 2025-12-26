8fbd1b686666df516993ab320e71cb9bbbed99d5
Andrew Laustrup <83102227+ALaustrup@users.noreply.github.com>
2025-12-26 10:27:17 -0600

    Await default export before accessing fetch

--- apps/web/serve.js
+++ apps/web/serve.js
@@ -1,7 +1,9 @@
 import { serve } from "@hono/node-server";
-import server from "./build/server/index.js";
+import serverModule from "./build/server/index.js";
 
 const port = Number(process.env.PORT) || 4000;
+
+const server = await serverModule.default;
 
 serve({
   fetch: server.fetch,
